d2359ba3a09ec254a981075233cd75c55a2766ab
Jasmine Carter <jasmine.carter87@gmail.com>
2019-06-02 18:27:21 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,11 @@
 def my_each(array)# put argument(s) here
-  count = 0
-  while count > array.length
-    array do |i|
-      puts i
+  if block_give?
+    count = 0
+    while count > array.length
+      array do |i|
+        puts i
+  else
+    puts "This block should not run!"
   end
   # code here
 end
