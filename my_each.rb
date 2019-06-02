b6f68faaba838248e7f15922c59aacab9ddaec87
Jasmine Carter <jasmine.carter87@gmail.com>
2019-06-02 18:48:20 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,7 @@
 def my_each(array)# put argument(s) here
   if block_given?
     count = 0
-    while count > array.length
+    while count < array.length
       yield array |i|
        i = i + 1
     end
