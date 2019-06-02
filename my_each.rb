470aab4bb1ceb20fad51c63c9c18dc055a6646ae
Jasmine Carter <jasmine.carter87@gmail.com>
2019-06-02 18:34:58 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -4,9 +4,8 @@ def my_each(array)# put argument(s) here
     while count > array.length
       yield array do |i|
         puts i
-    end    
+    end
     else
       puts "This block should not run!"
     end  # code here
-  end
 end
